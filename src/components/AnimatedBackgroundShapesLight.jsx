abb2e12f34a00e242c38068a9943dcd9d1a9caaa
Salman-sajib <sfsajib@gmail.com>
2024-05-10 00:08:49 +0600

    added a scroll message

--- src/components/AnimatedBackgroundShapesLight.jsx
+++ src/components/AnimatedBackgroundShapesLight.jsx
@@ -6,6 +6,9 @@ function AnimatedBackgroundShapesLight() {
         <div className=' absolute top-0 right-14 w-72 h-72 bg-yellow-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000 '></div>
         <div className=' absolute top-8 right-[150px] w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000 '></div>
       </div>
+      <h2 className=' absolute bottom-5 left-5 px-6 py-3 bg-gradient-to-r from-lime-400 to-teal-400 rounded-md text-lg font-medium '>
+        Scroll Down
+      </h2>
     </div>
   );
 }
